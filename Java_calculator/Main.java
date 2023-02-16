9bb12007d43fdc4e73d0d5a7e0e9e0d796a15065
PesnyaRadugi <mbarazgov02@gmail.com>
2023-02-16 22:09:37 +0300

    Quick fixes and refactoring
    Still no romanian numbers

--- Java_calculator/Main.java
+++ Java_calculator/Main.java
@@ -7,20 +7,18 @@ public class Main {
     {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println(calc(scanner.nextLine()));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static String calc(String input)
+    public static String calc(String input) throws Exception
     {
         String result = null;
         String[] expression = input.split(" ");
         if (expression.length != 3)
         {
-            try {
-                throw new Exception("Invalid number of elements in equasion, make sure all elements are separated by whitespace");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            throw new Exception("Invalid number of elements in equasion, make sure all elements are separated by whitespace");
         }
         
         switch(expression[1])
@@ -35,14 +33,17 @@ public class Main {
                 result = Integer.toString(Integer.parseInt(expression[0]) * Integer.parseInt(expression[2]));
                 break;
             case "/":
-                result = Integer.toString(Integer.parseInt(expression[0]) / Integer.parseInt(expression[2]));
+                if (Integer.parseInt(expression[2]) != 0)
+                {
+                    result = Integer.toString(Integer.parseInt(expression[0]) / Integer.parseInt(expression[2]));
+                }
+                else
+                {
+                    throw new Exception("Can't divide by 0");
+                }
                 break;
             default:
-                try {
-                    throw new Exception("Invalid operation");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                throw new Exception("Invalid operation");
             }
        
         return result;
